902b6aadda77f278c1a8188610fe6f0ccf5b9506
twinson37 <twinson37@hanyang.ac.kr>
2023-03-30 15:06:24 +0900

    Add MemberServiceTest

--- socksAI/src/main/java/MismatchedSocks/socksAI/domain/Member.java
+++ socksAI/src/main/java/MismatchedSocks/socksAI/domain/Member.java
@@ -27,6 +27,17 @@ public class Member {
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<Img> img = new ArrayList<>();
 
+    public Member() {
+    }
+
+    public Member(Long id, String user_id, String password, String name, String phonenumber, String address) {
+        this.user_id = user_id;
+        this.password = password;
+        this.name = name;
+        this.phonenumber = phonenumber;
+        this.address = address;
+    }
+
 //    public Member(String user_id, String password) {
 //        this.user_id = user_id;
 //        this.password = password;
