3a39c807c0f6b10f3aff92c1df7250cc0f22d22f
twinson37 <twinson37@hanyang.ac.kr>
2023-05-04 16:36:53 +0900

    Modifying hub.py

--- hub.py
+++ hub.py
@@ -20,6 +20,7 @@ im = f'{home_path}/img/{arg[1]}'
 results = model(im)
 
 # Results
+results.save()  # or .show(), .save(), .crop(), .pandas(), etc.
 results.crop()  # or .show(), .save(), .crop(), .pandas(), etc.
 
 # results.show()
@@ -41,35 +42,35 @@ print(results.pandas().xyxy[0])
 img_path = os.path.join(home_path,"runs/detect/exp/crops/sock/")
 meta_path = os.path.join(home_path,"metadata-files")
 
-dl = Load_Data()
-image_list = dl.from_folder([img_path])
-
-num = len(image_list)
-
-mathced = list()
+# dl = Load_Data()
+# image_list = dl.from_folder([img_path])
+#
+# num = len(image_list)
+#
+# mathced = list()
 # 발견 양말 없을 때 하나일 때 두개일때?
 # dirListing = os.listdir(img_path)
 
-for _ in range(num):
-    if(os.path.isdir(meta_path)):
-        shutil.rmtree(meta_path)
-
-    st = Search_Setup(image_list, model_name="vgg19", pretrained=True, image_count=None)
-    st.run_index()
-    # print(st.get_similar_images(image_path=image_list[0], number_of_images=2))
-    temp = st.get_similar_images(image_path=image_list[0], number_of_images=2)
-    temp.pop(0)
-
-    mathced.append([image_list[0],list(temp.values())[0]])
-
-    image_list.remove(image_list[0])
-    image_list.remove(list(temp.values())[0])
-    
-    # print(image_list)
-    new_num = len(image_list)
-    if (new_num==0) or (new_num==1): 
-        break;
-
-print(mathced)
+# for _ in range(num):
+#     if(os.path.isdir(meta_path)):
+#         shutil.rmtree(meta_path)
+#
+#     st = Search_Setup(image_list, model_name="vgg19", pretrained=True, image_count=None)
+#     st.run_index()
+#     # print(st.get_similar_images(image_path=image_list[0], number_of_images=2))
+#     temp = st.get_similar_images(image_path=image_list[0], number_of_images=2)
+#     temp.pop(0)
+#
+#     mathced.append([image_list[0],list(temp.values())[0]])
+#
+#     image_list.remove(image_list[0])
+#     image_list.remove(list(temp.values())[0])
+#
+#     # print(image_list)
+#     new_num = len(image_list)
+#     if (new_num==0) or (new_num==1):
+#         break;
+#
+# print(mathced)
 
 # print(f"hub = {time() -start}")
